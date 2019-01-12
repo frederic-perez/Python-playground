2fe97226a96e56d59c5ecb84401ea9fdd5897e4d
Frederic Perez <frederic-perez@users.noreply.github.com>
2019-01-12 12:26:02 +0100

    master :hammer: Added TODO tasks in comment

--- playground.py
+++ playground.py
@@ -15,3 +15,16 @@ assert average(math.pi) == math.pi
 assert average(1, 2) == 1.5
 
 assert linalg.det(matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])) == 1
+
+"""
+TODO
+
+class Point. Perhaps in numpy?
+class Sphere (center, radius)
+   print/output
+   bool point_is_on_surface(point, epsilon_distance)
+def get_sphere(4 points):
+   ...
+   return sphere
+
+"""
