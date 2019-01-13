b30da97debc0d29e81dc525acb6642390f2cf1a6
Frederic Perez <frederic-perez@users.noreply.github.com>
2019-01-13 11:26:34 +0100

    master :hammer: Add Sphere class # WIP

--- playground.py
+++ playground.py
@@ -18,13 +18,22 @@ assert linalg.det(matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])) == 1
 
 """
 TODO
-
-class Point. Perhaps in numpy?
-class Sphere (center, radius)
-   print/output
-   bool point_is_on_surface(point, epsilon_distance)
-def get_sphere(4 points):
-   ...
-   return sphere
-
+class Point (?). Perhaps in numpy? In scipy?
 """
+
+class Sphere(object):
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
+
+    def print:
+        TODO
+
+    def point_is_on_surface(point, epsilon_distance):
+        TODO
+        return False
+        
+def get_sphere(4 points):
+   TODO
+   Sphere sphere(vec(0, 0, 0), 0)
+   return sphere
