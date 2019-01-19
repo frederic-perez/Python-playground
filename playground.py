adf6a32025240c7efc54c3468a7e228ac2c2f9f0
Frederic Perez <frederic-perez@users.noreply.github.com>
2019-01-19 16:35:44 +0100

    master :hammer: Finish `get_sphere` implementation

--- playground.py
+++ playground.py
@@ -51,13 +51,43 @@ def get_sphere(points):
         a[i][1] = points[i][1]
         a[i][2] = points[i][2]
         a[i][3] = 1
-    
     minor_11 = np.linalg.det(a)
     if (minor_11 == 0):
         return Sphere(np.array([0, 0, 0], np.float_), 0.)
 
-    sphere = Sphere(np.array([0, 0, 0], np.float_), 1.)
-    return sphere
+    for i in range(0, 4):
+        a[i][0] = points[i][0]**2 + points[i][1]**2 + points[i][2]**2
+        a[i][1] = points[i][1]
+        a[i][2] = points[i][2]
+        a[i][3] = 1
+    minor_12 = np.linalg.det(a)
+    
+    for i in range(0, 4):
+        a[i][0] = points[i][0]**2 + points[i][1]**2 + points[i][2]**2
+        a[i][1] = points[i][0]
+        a[i][2] = points[i][2]
+        a[i][3] = 1
+    minor_13 = np.linalg.det(a)
+    
+    for i in range(0, 4):
+        a[i][0] = points[i][0]**2 + points[i][1]**2 + points[i][2]**2
+        a[i][1] = points[i][0]
+        a[i][2] = points[i][1]
+        a[i][3] = 1
+    minor_14 = np.linalg.det(a)
+    
+    for i in range(0, 4):
+        a[i][0] = points[i][0]**2 + points[i][1]**2 + points[i][2]**2
+        a[i][1] = points[i][0]
+        a[i][2] = points[i][1]
+        a[i][3] = points[i][2]
+    minor_15 = np.linalg.det(a)
+    
+    x = .5 * minor_12 / minor_11
+    y = -.5 * minor_13 / minor_11
+    z = .5 * minor_14 / minor_11
+    r = math.sqrt(x*x + y*y + z*z - minor_15/minor_11)
+    return Sphere(np.array([x, y, z], np.float_), r)
 
 point_A = np.array([1, 3, 6], np.float_)
 point_B = np.array([1, 3, 0], np.float_)
