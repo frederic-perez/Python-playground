dd817f94320423cbe9d4619b9695d4d7653d07b6
Frederic Perez <frederic.perez@gmx.us>
2019-01-23 12:15:55 +0100

    master :hammer: First baby steps for TDD

---
+++ test_playground.py
@@ -0,0 +1,20 @@
+"""
+Run the tests by executing:
+$ python -m unittest -v test_playground
+"""
+
+import math
+import unittest
+from playground import average
+
+class TestAverage(unittest.TestCase):
+
+    def test_WHEN_single_parameter_THEN_result_must_be_equal_to_parameter(self):
+        self.assertEqual(average(1), 1)
+        self.assertEqual(average(math.pi), math.pi)
+
+    def test_WHEN_two_parameters_THEN_result_must_be_equal_to_half_their_sum(self):
+        self.assertEqual(average(1, 2), (1+2)/2.)
+
+if __name__ == '__main__':
+    unittest.main()
