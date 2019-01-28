f1955ec58e982ee0c39c604cecd34c9e380f4a46
Frederic Perez <frederic.perez@gmx.us>
2019-01-28 15:44:59 +0100

    master :hammer: Fix names of tests (names of methods)

--- test_sphere.py
+++ test_sphere.py
@@ -10,21 +10,21 @@ from sphere import Sphere, get_sphere, epsilon_distance
 
 class Test_Sphere(unittest.TestCase):
 
-    def test_WHEN_setting_two_spheres_with_the_same_parameters_WHEN_comparing_them_THEN_result_is_true(self):
+    def test_GIVEN_two_spheres_created_with_the_same_parameters_WHEN_comparing_them_THEN_result_is_true(self):
         POINT = np.array([1, 3, 6], np.float_)
         RADIUS = 3.
         self.assertEqual(Sphere(POINT, RADIUS), Sphere(POINT, RADIUS))
         SPHERE = Sphere(POINT, RADIUS)
         self.assertEqual(SPHERE, Sphere(POINT, RADIUS))
 
-    def test_WHEN_setting_two_spheres_with_different_parameters_WHEN_comparing_them_THEN_result_is_false(self):
+    def test_GIVEN_two_spheres_created_with_different_parameters_WHEN_comparing_them_THEN_result_is_false(self):
         POINT_A = np.array([1, 3, 6], np.float_)
         RADIUS = 3.
         self.assertNotEqual(Sphere(POINT_A, RADIUS), Sphere(POINT_A, RADIUS + 1.))
         POINT_B = np.array([4, 3, 3], np.float_)
         self.assertNotEqual(Sphere(POINT_A, RADIUS), Sphere(POINT_B, RADIUS))
 
-    def test_WHEN_setting_a_sphere_with_a_given_radius_R_WHEN_calling_get_radius_THEN_result_is_R(self):
+    def test_GIVEN_a_sphere_created_with_radius_R_WHEN_calling_get_radius_THEN_result_is_R(self):
         POINT = np.array([1, 2, 3], np.float_)
         RADIUS = 7
         SPHERE = Sphere(POINT, RADIUS)
@@ -33,11 +33,11 @@ class Test_Sphere(unittest.TestCase):
 
 class Test_get_sphere(unittest.TestCase):
 
-    def test_WHEN_calling_get_sphere_with_a_wrong_sphere_WHEN_calling_get_sphere_THEN_its_radius_must_be_0(self):
+    def test_GIVEN_a_point_WHEN_calling_get_sphere_with_that_point_4x__and_get_radius_THEN_result_is_0(self):
         POINT = np.array([1, 3, 6], np.float_)
         self.assertEqual(get_sphere((POINT, POINT, POINT, POINT)).get_radius(), 0.)
 
-    def test_WHEN_calling_get_sphere_with_31_136_12_106_data_THEN_result_must_be_equal_to_that_of_the_original_site(self):
+    def test_GIVEN_4_points_from_31_136_12_106_WHEN_calling_get_sphere_using_them_THEN_result_must_be_equal_to_that_of_the_original_site(self):
         POINT_1 = np.array([-34.1186,  1.389,   8.5034], np.float_)
         POINT_2 = np.array([-34.3179,  1.3719, -29.432], np.float_)
         POINT_3 = np.array([-8.7948,  -0.1148, -10.462], np.float_)
@@ -49,7 +49,7 @@ class Test_get_sphere(unittest.TestCase):
         RADIUS_31_136_12_106 = 114.39638
         self.assertEqual(SPHERE_31_136_12_106, Sphere(CENTER_31_136_12_106, RADIUS_31_136_12_106))
 
-    def test_WHEN_calling_get_sphere_with_39_136_10_106_data_THEN_result_must_be_equal_to_that_of_the_original_site(self):
+    def test_GIVEN_4_points_from_39_136_10_106_WHEN_calling_get_sphere_using_them_THEN_result_must_be_equal_to_that_of_the_original_site(self):
         POINT_1 = np.array([-34.5737, 0.5015,  10.377], np.float_)
         POINT_2 = np.array([-34.4944, 0.4966, -22.06], np.float_)
         POINT_3 = np.array([-8.2244,  0.5713,  -5.6182], np.float_)
