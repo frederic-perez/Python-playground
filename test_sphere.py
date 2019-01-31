7a47a696430fd457d7a95f28487205ba0f956672
Frederic Perez <frederic.perez@gmx.us>
2019-01-31 11:10:37 +0100

    master :hammer: Improve names of tests # Done

--- test_sphere.py
+++ test_sphere.py
@@ -10,34 +10,34 @@ from sphere import Sphere, get_sphere, epsilon_distance
 
 class Test_Sphere(unittest.TestCase):
 
-    def test_GIVEN_two_spheres_created_with_the_same_parameters_WhenComparision_ThenReturnTrue(self):
+    def test_Given2SpheresCreatedEqually_WhenComparision_ThenReturnTrue(self):
         POINT = np.array([1, 3, 6], np.float_)
         RADIUS = 3.
         self.assertEqual(Sphere(POINT, RADIUS), Sphere(POINT, RADIUS))
         SPHERE = Sphere(POINT, RADIUS)
         self.assertEqual(SPHERE, Sphere(POINT, RADIUS))
 
-    def test_GIVEN_two_spheres_created_with_different_parameters_WhenComparision_ThenReturnFalse(self):
+    def test_Given2SpheresCreatedNotEqually_WhenComparision_ThenReturnFalse(self):
         POINT_A = np.array([1, 3, 6], np.float_)
         RADIUS = 3.
         self.assertNotEqual(Sphere(POINT_A, RADIUS), Sphere(POINT_A, RADIUS + 1.))
         POINT_B = np.array([4, 3, 3], np.float_)
         self.assertNotEqual(Sphere(POINT_A, RADIUS), Sphere(POINT_B, RADIUS))
 
-    def test_GIVEN_a_sphere_created_with_radius_R_When_get_radius_ThenReturnR(self):
+    def test_GivenSphereCreatedWithRadiusR_When_get_radius_ThenReturnR(self):
         POINT = np.array([1, 2, 3], np.float_)
         RADIUS = 7
         SPHERE = Sphere(POINT, RADIUS)
-        SPHERE.spy("SPHERE")
+        # SPHERE.spy("SPHERE")
         self.assertEqual(SPHERE.get_radius(), RADIUS)
 
 class Test_get_sphere(unittest.TestCase):
 
-    def test_GIVEN_a_point_When_get_sphere_WithThatPoint4x_and_get_radius_ThenReturn0(self):
+    def test_GivenAPointP_When_get_sphere_WithP4x_and_get_radius_ThenReturn0(self):
         POINT = np.array([1, 3, 6], np.float_)
         self.assertEqual(get_sphere((POINT, POINT, POINT, POINT)).get_radius(), 0.)
 
-    def test_GIVEN_4_points_from_31_136_12_106_When_get_sphere_ThenReturnTheSitesResult(self):
+    def test_Given4PointsFrom_31_136_12_106_When_get_sphere_ThenReturnTheSitesResult(self):
         POINT_1 = np.array([-34.1186,  1.389,   8.5034], np.float_)
         POINT_2 = np.array([-34.3179,  1.3719, -29.432], np.float_)
         POINT_3 = np.array([-8.7948,  -0.1148, -10.462], np.float_)
@@ -51,7 +51,7 @@ class Test_get_sphere(unittest.TestCase):
 
         self.assertEqual(SPHERE_31_136_12_106, SPHERE_FROM_SITE)
 
-    def test_GIVEN_4_points_from_39_136_10_106_When_get_sphere_ThenReturnTheSitesResult(self):
+    def test_Given4PointsFrom_39_136_10_106_When_get_sphere_ThenReturnTheSitesResult(self):
         POINT_1 = np.array([-34.5737, 0.5015,  10.377], np.float_)
         POINT_2 = np.array([-34.4944, 0.4966, -22.06], np.float_)
         POINT_3 = np.array([-8.2244,  0.5713,  -5.6182], np.float_)
@@ -65,21 +65,21 @@ class Test_get_sphere(unittest.TestCase):
 
         self.assertEqual(SPHERE_39_136_10_106, SPHERE_FROM_SITE)
 
-    def test_GIVEN_a_sphere_and_a_point_being_equal_to_center_plus_radius_for_x_When_point_is_on_surface_ThenReturnTrue(self):
+    def test_GivenSphereAndPointEqualToCenterPlusRadiusForX_When_point_is_on_surface_ThenReturnTrue(self):
         CENTER = np.array([1, 2, 3], np.float_)
         RADIUS = 7
         SPHERE = Sphere(CENTER, RADIUS)
         POINT = CENTER + np.array([RADIUS, 0, 0], np.float_)
         self.assertTrue(SPHERE.point_is_on_surface(POINT))
 
-    def test_GIVEN_a_sphere_and_a_point_almost_equal_to_center_plus_radius_for_x_When_point_is_on_surface_ThenReturnTrue(self):
+    def test_GivenSphereAndPointAlmostEqualToCenterPlusRadiusForX_When_point_is_on_surface_ThenReturnTrue(self):
         CENTER = np.array([1, 2, 3], np.float_)
         RADIUS = 7
         SPHERE = Sphere(CENTER, RADIUS)
         POINT = CENTER + np.array([RADIUS + epsilon_distance/2., 0, 0], np.float_)
         self.assertTrue(SPHERE.point_is_on_surface(POINT))
 
-    def test_GIVEN_a_sphere_and_a_point_far_enough_to_center_plus_radius_for_x_When_point_is_on_surface_ThenReturnFalse(self):
+    def test_GivenSphereAndPointFarEnoughToCenterPlusRadiusForX_When_point_is_on_surface_ThenReturnFalse(self):
         CENTER = np.array([1, 2, 3], np.float_)
         RADIUS = 7
         SPHERE = Sphere(CENTER, RADIUS)
