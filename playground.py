1116c3e90fc5bb38bd1bfd42a4fd0c81cde94cd5
Frederic Perez <frederic-perez@users.noreply.github.com>
2019-02-02 10:08:09 +0100

    master :hammer: Raise exception for bad input

--- playground.py
+++ playground.py
@@ -2,5 +2,8 @@
 
 def average(*numbers):
     """Self-explanatory"""
+    if not numbers:
+        raise ValueError('numbers should not be empty')
+
     numbers = [float(number) for number in numbers]
     return sum(numbers) / float(len(numbers))
