f2c3ba9a69bc0c4ebfbfaac7472706f302bf9d08
Frederic Perez <frederic-perez@users.noreply.github.com>
2019-02-09 12:44:40 +0100

    master :hammer: ENH class; add `get_base_curve`

--- optical_sphere.py
+++ optical_sphere.py
@@ -5,24 +5,29 @@ import numpy as np
 from sphere import Sphere
 
 class OpticalSphere(Sphere):
-    def __init__(self, center, radius, n):
-        Sphere.__init__(self, center, radius)
+    def __init__(self, radius, n):
+        CENTER = np.zeros(3)
+        Sphere.__init__(self, CENTER, radius)
         self.n = n
         return
 
     def __str__(self):
-        return 'OpticalSphere(center={0}, radius={1}, n={2})'.format(self.center, self.radius, self.n)
+        return 'OpticalSphere(radius={0}, n={1})'.format(self.radius, self.n)
 
     def get_n(self):
         return self.n
+
+    def get_base_curve(self):
+        N_VACUUM = 1
+        return (self.n - N_VACUUM)/self.radius
 
     def spy(self, message):
         print '{0}: {1}'.format(message, self)
         return
 
-CENTER = np.array([1, 2, 3], np.float_)
-RADIUS = 7
-N = 1.5
-OPTICAL_SPHERE = OpticalSphere(CENTER, RADIUS, N)
-OPTICAL_SPHERE.spy("OPTICAL_SPHERE")
-print "OPTICAL_SPHERE:", OPTICAL_SPHERE
+if __name__ == '__main__':
+    RADIUS = 0.1
+    N = 1.5
+    OPTICAL_SPHERE = OpticalSphere(RADIUS, N)
+    print "OPTICAL_SPHERE:", OPTICAL_SPHERE
+    print "  '- base curve is", OPTICAL_SPHERE.get_base_curve(), "diopters"
