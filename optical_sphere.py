82920cb3700b026cc43a6ccef07280efc732082b
Frederic Perez <frederic.perez@gmx.us>
2019-02-27 10:48:33 +0100

    master :hammer: Rework back `optical_sphere.py`

--- optical_sphere.py
+++ optical_sphere.py
@@ -5,27 +5,24 @@ import numpy as np
 from sphere import Sphere
 
 class OpticalSphere(Sphere):
-    def __init__(self, base_curve):
+    def __init__(self, radius):
         CENTER = np.zeros(3)
-        N = 1.53
-        N_VACUUM = 1
-        RADIUS = 1000*(N - N_VACUUM)/base_curve
-        Sphere.__init__(self, CENTER, RADIUS)
-        self.base_curve = base_curve
-        self.N = N
+        Sphere.__init__(self, CENTER, radius)
+        self.N = 1.53
         return
 
     def __str__(self):
-        return 'OpticalSphere(base_curve: {0} mm, n: {1})'.format(self.base_curve, self.N)
-
-    def get_base_curve(self):
-        return self.base_curve
+        return 'OpticalSphere(radius: {0} mm, n: {1})'.format(self.radius, self.N)
 
     def get_n(self):
         return self.N
         
     def get_surface_power(self):
         return 530/self.radius
+
+    def get_base_curve(self):
+        N_VACUUM = 1
+        return 1000*(self.N - N_VACUUM)/self.radius
 
     def spy(self, message):
         print '{0}: {1}'.format(message, self)
@@ -34,15 +31,14 @@ class OpticalSphere(Sphere):
 def print_optical_info(base_curve):
     OPTICAL_SPHERE = OpticalSphere(base_curve)
     print "OPTICAL_SPHERE:", OPTICAL_SPHERE
-    print "  |- base curve is", OPTICAL_SPHERE.get_base_curve(), "diopter(s)"
     print "  |- surface power is", OPTICAL_SPHERE.get_surface_power(), "diopter(s)"
-    print "  '- radius is", OPTICAL_SPHERE.get_radius(), "mm"
+    print "  '- base curve is", OPTICAL_SPHERE.get_base_curve(), "diopter(s)"
     print
 
 if __name__ == '__main__':
     
-    base_curve = 1 # 1 diopter curve
-    print_optical_info(base_curve)
+    radius = 530 # 530 mm is the radius of a 1 diopter curve
+    print_optical_info(radius)
 
-    base_curve = 5
-    print_optical_info(base_curve)
+    radius = 106
+    print_optical_info(radius)
--- test_optical_sphere.py
+++ test_optical_sphere.py
@@ -10,16 +10,20 @@ from optical_sphere import OpticalSphere
 class Test_OpticalSphere(unittest.TestCase):
 
     def test_Given2OpticalSpheresCreatedEqually_WhenComparision_ThenReturnTrue(self):
-        BASE = 1.
-        self.assertEqual(OpticalSphere(BASE), OpticalSphere(BASE))
-        OPTICAL_SPHERE = OpticalSphere(BASE)
-        self.assertEqual(OPTICAL_SPHERE, OpticalSphere(BASE))
+        RADIUS = 1.
+        self.assertEqual(OpticalSphere(RADIUS), OpticalSphere(RADIUS))
+        OPTICAL_SPHERE = OpticalSphere(RADIUS)
+        self.assertEqual(OPTICAL_SPHERE, OpticalSphere(RADIUS))
 
-    def test_GivenOpticalSphereWithBase1_When_get_radius_ThenReturn530(self):
-        # 530 mm is the radius of a 1 diopter curve
-        BASE = 1.
-        OPTICAL_SPHERE = OpticalSphere(BASE)
-        self.assertTrue(equal_in_practice(OPTICAL_SPHERE.get_radius(), 530))
+    def test_GivenOpticalSphereWithRadius530_When_get_base_curve_ThenReturn1(self):
+        RADIUS = 530 # 530 mm is the radius of a 1 diopter curve
+        OPTICAL_SPHERE = OpticalSphere(RADIUS)
+        self.assertTrue(equal_in_practice(OPTICAL_SPHERE.get_base_curve(), 1))
+
+    def test_GivenOpticalSphereWithRadius106_When_get_radius_ThenReturn5(self):
+        RADIUS = 106
+        OPTICAL_SPHERE = OpticalSphere(RADIUS)
+        self.assertTrue(equal_in_practice(OPTICAL_SPHERE.get_base_curve(), 5))
 
 if __name__ == '__main__':
     unittest.main()
