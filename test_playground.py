75c8e7aab4bde0af259d27a736ec40548cec414c
Frederic Perez <frederic.perez@gmx.us>
2019-02-27 10:49:19 +0100

    master :hammer: Petty ENHs

--- test_playground.py
+++ test_playground.py
@@ -5,6 +5,7 @@ $ python -m unittest -v test_playground
 
 import math
 import unittest
+from epsilon import equal_in_practice
 from playground import average
 
 class Test_average(unittest.TestCase):
@@ -13,11 +14,11 @@ class Test_average(unittest.TestCase):
         self.assertRaises(ValueError, average)
 
     def test_GivenSingleParameterP_When_average_ThenReturnP(self):
-        self.assertEqual(average(1), 1)
-        self.assertEqual(average(math.pi), math.pi)
+        self.assertTrue(equal_in_practice(average(1), 1))
+        self.assertTrue(equal_in_practice(average(math.pi), math.pi))
 
     def test_Given2Parameters_When_average_ThenReturnHalfTheirSum(self):
-        self.assertEqual(average(1, 2), (1+2)/2.)
+        self.assertTrue(equal_in_practice(average(1, 2), (1+2)/2.))
 
 if __name__ == '__main__':
     unittest.main()
