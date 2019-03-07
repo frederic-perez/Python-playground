f3371c147c4a442236276887fb3fa1b8454836c0
Frederic Perez <frederic.perez@gmx.us>
2019-03-07 09:54:11 +0100

    master :hammer: Simplify code; ENH logger messages; + `decompress_7z_file`

--- playground.py
+++ playground.py
@@ -21,67 +21,65 @@ def average(*numbers):
     numbers = [float(number) for number in numbers]
     return sum(numbers) / float(len(numbers))
 
-def get_sphere_given4StraightCrosshairPoints_From_42():
-    POINT_1 = np.array([-35.3025, 0.6357,   3.8584], np.float_)
-    POINT_2 = np.array([-35.0932, 1.3599, -43.2535], np.float_)
-    POINT_3 = np.array([ -9.054, -0.958,  -19.6768], np.float_)
-    POINT_4 = np.array([-62.0412, 5.9778, -19.7658], np.float_)
-    POINTS = (POINT_1, POINT_2, POINT_3, POINT_4)
-    SPHERE_42 = get_sphere(POINTS)
-    logger.info('SPHERE_42 given 4 straight crosshair points: %s', SPHERE_42)
-
-def get_sphere_given4RotatedCrosshairPoints_From_42():
-    POINT_1 = np.array([-58.7767,  4.9348,    -3.7779], np.float_)
-    POINT_2 = np.array([-13.3565, -0.644701, -29.8066], np.float_)
-    POINT_3 = np.array([-16.4319, -0.999301,   1.6949], np.float_)
-    POINT_4 = np.array([-55.543,   4.7384,   -37.1852], np.float_)
-    POINTS = (POINT_1, POINT_2, POINT_3, POINT_4)
-    SPHERE_42 = get_sphere(POINTS)
-    logger.info('SPHERE_42 given 4 rotated crosshair points: %s', SPHERE_42)
-
-def get_sphere_given4StraightCrosshairPoints_From_46():
-    POINT_1 = np.array([-36.9284,  2.1009,  -1.0189], np.float_)
-    POINT_2 = np.array([-37.0922,  2.2078, -51.4502], np.float_)
-    POINT_3 = np.array([-10.4147, -0.1741, -26.0583], np.float_)
-    POINT_4 = np.array([-65.5153,  8.0756, -26.2123], np.float_)
-    POINTS = (POINT_1, POINT_2, POINT_3, POINT_4)
-    SPHERE_46 = get_sphere(POINTS)
-    logger.info('SPHERE_46 given 4 straight crosshair points: %s', SPHERE_46)
-
-def get_sphere_given4RotatedCrosshairPoints_From_46():
-    POINT_1 = np.array([-56.2434, 5.6835, -8.3162], np.float_)
-    POINT_2 = np.array([-15.5323, 0.0641003, -37.5089], np.float_)
-    POINT_3 = np.array([-54.5831, 5.3886, -44.8675], np.float_)
-    POINT_4 = np.array([-18.2069, 0.2075, -3.8871], np.float_)
-    POINTS = (POINT_1, POINT_2, POINT_3, POINT_4)
-    SPHERE_46 = get_sphere(POINTS)
-    logger.info('SPHERE_46 given 4 rotated crosshair points: %s', SPHERE_46)
+def get_sphere_given4Points(description, points):
+    SPHERE = get_sphere(points)
+    logger.info('Sphere given %s: %s', description, SPHERE)
 
 def get_git_version():
-    COMMAND = "git --version"
+    COMMAND = 'git --version'
     timer = Timer()
     RESULT = subprocess.call(COMMAND, shell=True)  # returns the exit code in unix
-    logger.info('Command \"%s\" took %s and returned %d', COMMAND, timer.get_duration_string(), RESULT)
+    logger.info('Command `%s` took %s and returned %d', COMMAND, timer.get_duration_string(), RESULT)
 
 def download_file_using_curl(url, output_filename):
-    COMMAND = "curl " + url + " -o " + output_filename
+    COMMAND = 'curl ' + url + ' -o ' + output_filename
     timer = Timer()
     RESULT = subprocess.call(COMMAND, shell=True)  # returns the exit code in unix
     if RESULT == 0:
-        logger.info('File "%s" has been downloaded in %s', output_filename, timer.get_duration_string())
+        logger.info('File `%s` has been downloaded in %s', output_filename, timer.get_duration_string())
     else:
-        logger.error('Failed to download file "%s"', output_filename)
+        logger.error('Failed to download file `%s`', output_filename)
+
+def decompress_7z_file(filename, output_directory):
+    COMMAND = '7z x ' + filename + ' -o' + output_directory
+    timer = Timer()
+    RESULT = subprocess.call(COMMAND, shell=True)  # returns the exit code in unix
+    logger.info('Command `%s` took %s and returned %d', COMMAND, timer.get_duration_string(), RESULT)
 
 if __name__ == '__main__':
 
-    get_sphere_given4StraightCrosshairPoints_From_42()
-    get_sphere_given4RotatedCrosshairPoints_From_42()
-
-    get_sphere_given4StraightCrosshairPoints_From_46()
-    get_sphere_given4RotatedCrosshairPoints_From_46()
+    ARRAY_OF_DESCRIPTION_AND_POINTS = [
+        ('4 straight crosshair points from whatnot 42', [
+            [-35.3025, 0.6357,   3.8584],
+            [-35.0932, 1.3599, -43.2535],
+            [ -9.054, -0.958,  -19.6768],
+            [-62.0412, 5.9778, -19.7658]
+        ]),
+        ('4 rotated crosshair points from whatnot 42', [
+            [-58.7767,  4.9348,    -3.7779],
+            [-13.3565, -0.644701, -29.8066],
+            [-16.4319, -0.999301,   1.6949],
+            [-55.543,   4.7384,   -37.1852]
+        ]),
+        ('4 straight crosshair points from whatnot 46', [
+            [-36.9284,  2.1009,  -1.0189],
+            [-37.0922,  2.2078, -51.4502],
+            [-10.4147, -0.1741, -26.0583],
+            [-65.5153,  8.0756, -26.2123]
+        ]),
+        ('4 rotated crosshair points from whatnot 46', [
+            [-56.2434, 5.6835, -8.3162],
+            [-15.5323, 0.0641003, -37.5089],
+            [-54.5831, 5.3886, -44.8675],
+            [-18.2069, 0.2075, -3.8871]
+        ])
+    ]
+    for description, points in ARRAY_OF_DESCRIPTION_AND_POINTS:
+        get_sphere_given4Points(description, points)
 
     get_git_version()
 
+    """
     FILENAME_AND_URL_ARRAY = [
       # ['data/_L.7z', 'https://ucfc43a66550d50a72d5c6ad893966.dl.dropboxusercontent.com/cd/0/get/Acgtbdfxtq1Hv_TCylXob8zA8RNVbg9qJNZUkuBJ9GrD039rGB4SQ4vvTlDHCw_Wvzxivl1re-R_i-HM_dkKPzcpowNgxikC8ot3SkB2Y7_gSMYBVN8gxsCMzExKMUvxqvU/file#'],
       # ['data/_M.7z', 'https://ucfa13937452aeb2f165f01ea16937.dl.dropboxusercontent.com/cd/0/get/Aclm3eKtglwJbgFkBAX8u_8Lbf1ZkoqfgchqjzGEXju8LUjgHyIfRGKfKnpHg1opTaisrztJFu8Sda-S3He6zgthINXLc5dpwJUo8dqaCX1SE5dbpdQoMsffGxqe8hmsg_k/file?dl=1#'],
@@ -93,6 +91,8 @@ if __name__ == '__main__':
     for filename_and_url in FILENAME_AND_URL_ARRAY:
         filename, url = filename_and_url
         download_file_using_curl(url, filename)
-        # decompress_7z_file
-        # generate_sizes
+    """
+    filename = 'data/_M.7z'
+    output_directory = 'data/_M'
+    # decompress_7z_file(filename, output_directory)
   
