1638613d26bbf8891a5ebbdec836db9b26cc096c
Frederic Perez <frederic-perez@users.noreply.github.com>
2019-03-09 11:49:42 +0100

    master :hammer: Avoid code duplication

--- test_error_array.py
+++ test_error_array.py
@@ -26,25 +26,16 @@ class Test_get_index_of_minimum_abs_error(unittest.TestCase):
         self.assertEqual(get_index_of_minimum_abs_error(ERRORS), 0)
 
     def test_GivenAnArrayOfAscendingAbsoluteValues_When_get_index_of_minimum_abs_error_ThenReturn0(self):
-        errors = [1, 3, 5]
-        self.assertEqual(get_index_of_minimum_abs_error(errors), 0)
-
-        errors = [-1, -3, -5]
-        self.assertEqual(get_index_of_minimum_abs_error(errors), 0)
+        for errors in [[1, 3, 5], [-1, -3, -5]]:
+            self.assertEqual(get_index_of_minimum_abs_error(errors), 0)
 
     def test_GivenAnArrayOfNDescendingAbsoluteValues_When_get_index_of_minimum_abs_error_ThenReturnNMinus1(self):
-        errors = [5, 3, 1]
-        self.assertEqual(get_index_of_minimum_abs_error(errors), len(errors) - 1)
-
-        errors = [-5, -3, -1]
-        self.assertEqual(get_index_of_minimum_abs_error(errors), len(errors) - 1)
+        for errors in [[5, 3, 1], [-5, -3, -1]]:
+            self.assertEqual(get_index_of_minimum_abs_error(errors), len(errors) - 1)
 
     def test_GivenAnArrayWithTheMinimumAbsoluteValueInTheMiddle_When_get_index_of_minimum_abs_error_ThenReturnNMinus1Div2(self):
-        errors = [5, 1, 3]
-        self.assertEqual(get_index_of_minimum_abs_error(errors), (len(errors) - 1)/2)
-
-        errors = [-5, -1, -3]
-        self.assertEqual(get_index_of_minimum_abs_error(errors), (len(errors) - 1)/2)
+        for errors in [[5, 1, 3], [-5, -1, -3]]:
+            self.assertEqual(get_index_of_minimum_abs_error(errors), (len(errors) - 1)/2)
 
 class Test_get_indices_around_minimum_abs_error(unittest.TestCase):
 
@@ -65,31 +56,19 @@ class Test_get_indices_around_minimum_abs_error(unittest.TestCase):
         self.assertEqual(get_indices_around_minimum_abs_error(ERRORS), (0, 1))
 
     def test_GivenAnArrayOfAscendingAbsoluteValues_When_get_indices_around_minimum_abs_error_ThenReturn0And1(self):
-        errors = [1, 3, 5]
-        self.assertEqual(get_indices_around_minimum_abs_error(errors), (0, 1))
-
-        errors = [-1, -3, -5]
-        self.assertEqual(get_indices_around_minimum_abs_error(errors), (0, 1))
+        for errors in [[1, 3, 5], [-1, -3, -5]]:
+            self.assertEqual(get_indices_around_minimum_abs_error(errors), (0, 1))
 
     def test_GivenAnArrayOfNDescendingAbsoluteValues_When_get_indices_around_minimum_abs_error_ThenReturnNMinus2AndNMinus1(self):
-        errors = [5, 3, 1]
-        n = len(errors)
-        self.assertEqual(get_indices_around_minimum_abs_error(errors), (n - 2, n - 1))
-
-        errors = [-5, -3, -1]
-        n = len(errors)
-        self.assertEqual(get_indices_around_minimum_abs_error(errors), (n - 2, n - 1))
+        for errors in [[5, 3, 1], [-5, -3, -1]]:
+            n = len(errors)
+            self.assertEqual(get_indices_around_minimum_abs_error(errors), (n - 2, n - 1))
 
     def test_GivenAnArrayWithTheMinimumAbsoluteValueInTheMiddle_When_get_indices_around_minimum_abs_error_ThenReturnIndicesAroundCenter(self):
-        errors = [5, 1, 3]
-        n = len(errors)
-        idx_center = (n - 1)/2
-        self.assertEqual(get_indices_around_minimum_abs_error(errors), (idx_center - 1, idx_center + 1))
-
-        errors = [-5, -1, -3]
-        n = len(errors)
-        idx_center = (n - 1)/2
-        self.assertEqual(get_indices_around_minimum_abs_error(errors), (idx_center - 1, idx_center + 1))
+        for errors in [[5, 1, 3], [-5, -1, -3]]:
+            n = len(errors)
+            idx_center = (n - 1)/2
+            self.assertEqual(get_indices_around_minimum_abs_error(errors), (idx_center - 1, idx_center + 1))
 
 if __name__ == '__main__':
     unittest.main()
