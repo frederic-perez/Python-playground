4291e872b604d2fe4d8a2d90bdd31c91a40a79a5
Frederic Perez <frederic.perez@gmx.us>
2019-03-15 13:37:37 +0100

    master :hammer: Add `check_not_empty`

--- check.py
+++ check.py
@@ -6,3 +6,7 @@ def check_array_type(parameter):
 
 def is_an_array(parameter):
     return hasattr(parameter, "__len__") and not isinstance(parameter, str)
+
+def check_not_empty(parameter):
+    if parameter.__len__() == 0:
+        raise TypeError('The parameter should not be empty')
--- test_check.py
+++ test_check.py
@@ -5,7 +5,7 @@ Run the tests by executing, for all test classes:
 """
 
 import unittest
-from check import check_array_type, is_an_array
+from check import check_array_type, is_an_array, check_not_empty
 
 class Test_check_array_type(unittest.TestCase):
 
@@ -18,6 +18,16 @@ class Test_is_an_array(unittest.TestCase):
     def test_GivenANotAnArray_When_is_an_array_ThenReturnFalse(self):
         ERRORS = 'error'
         self.assertFalse(is_an_array(ERRORS))
+
+class Test_not_empty(unittest.TestCase):
+
+    def test_GivenAnEmptyArray_When_check_not_empty_ThenExceptionIsRaised(self):
+        array = []
+        self.assertRaises(TypeError, check_not_empty, array)
+
+    def test_GivenANotEmptyArray_When_check_not_empty_ThenReturnNone(self):
+        array = [1]
+        self.assertEqual(check_not_empty(array), None)
 
 if __name__ == '__main__':
     unittest.main()
