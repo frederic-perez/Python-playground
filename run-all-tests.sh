8892e5829d56a828320e6583e935c7cc9102d94c
Frederic Perez <frederic.perez@gmx.us>
2019-03-18 10:47:43 +0100

    master :hammer: Don't use `-v` for a + compact output

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -2,4 +2,5 @@
 #   $ sh run-all-tests.sh
 # to run all tests
 #
-python -m unittest -v `ls test_*.py | sed 's/.py//'`
+# python -m unittest -v `ls test_*.py | sed 's/.py//'`
+python -m unittest `ls test_*.py | sed 's/.py//'`
