cb33c95484e7ea79a139f4c238b5fb12bfc8ae13
Frederic Perez <frederic.perez@gmx.us>
2019-04-12 12:17:39 +0200

    master :hammer: Petty ENH

--- test_check.py
+++ test_check.py
@@ -109,16 +109,20 @@ class Test_length_is_greater_than_N(unittest.TestCase):
 class Test_length_is_greater_or_equal_to_N(unittest.TestCase):
 
     def test_GivenANotEmptyArrayAndNLowerOrEqualToItsSize_When_length_is_greater_or_equal_to_N_ThenReturnNone(self):
-        array = [1, 2, 3]
-        SIZE = len(array)
-        for n in range(0, SIZE + 1):
-            self.assertEqual(check.length_is_greater_or_equal_to_N(array, n), None)
+        array = []
+        for i in range(0, 5):
+            array.append(i)
+            size = len(array)
+            for n in range(0, size + 1):
+                self.assertEqual(check.length_is_greater_or_equal_to_N(array, n), None)
 
     def test_GivenANotEmptyArrayAndNLargerThanItsSize_When_length_is_greater_or_equal_to_N_ThenExceptionIsRaised(self):
-        array = [1, 2, 3]
-        SIZE = len(array)
-        for n in range(SIZE + 1, SIZE + 5):
-            self.assertRaises(ValueError, check.length_is_greater_or_equal_to_N, array, n)
+        array = []
+        for i in range(0, 5):
+            array.append(i)
+            size = len(array)
+            for n in range(size + 1, size + 5):
+                self.assertRaises(ValueError, check.length_is_greater_or_equal_to_N, array, n)
 
 if __name__ == '__main__':
     unittest.main()
