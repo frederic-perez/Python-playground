5fe3afa8b207d5ffae906101f08de15cdeb21034
Frederic Perez <frederic.perez@gmx.us>
2019-05-27 11:26:37 +0200

    master :hammer: Petty change

--- epsilon.py
+++ epsilon.py
@@ -7,3 +7,7 @@ def zero_in_practice(float, epsilon = epsilon_distance):
 
 def equal_in_practice(float_1, float_2, epsilon = epsilon_distance):
     return abs(float_1 - float_2) <= epsilon
+
+if __name__ == '__main__':
+    F = 12.3456789e-5
+    print('zero_in_practice({}) returns {}'.format(F, zero_in_practice(F)))
