88e77b6de12c2cc31f1069e1e2be48f315c6ae10
Frederic Perez <frederic.perez@gmx.us>
2023-05-01 19:47:02 +0200

    🔨Convert floats to ints using `numpy.astype(int)`

--- wip.py
+++ wip.py
@@ -7,23 +7,25 @@ import numpy as np
 
 def main():
     for v in range(1, 3):
-        print(f'{v%2} {np.floor(v/2):.0f}')
+        print(f'{v%2} {np.floor(v/2).astype(int)}')
 
     print()
 
     for v in range(1, 4):
-        print(f'{(v%3)%2} {np.floor((v%3)/2):.0f} {np.floor(v/3):.0f}')
+        print(f'{(v%3)%2} {np.floor((v%3)/2).astype(int)} {np.floor(v/3).astype(int)}')
 
     print()
 
     for v in range(1, 5):
-        print(f'{((v%4)%3)%2} {np.floor(((v%4)%3)/2):.0f} {np.floor((v%4)/3):.0f} {np.floor(v/4):.0f}')
+        print(f'{((v%4)%3)%2} {np.floor(((v%4)%3)/2).astype(int)} {np.floor((v%4)/3).astype(int)} '
+              f'{np.floor(v/4).astype(int)}')
 
     print()
 
     for v in range(1, 6):
-        print(f'{(((v % 5) % 4) % 3) % 2} {np.floor((((v % 5) % 4) % 3) / 2):.0f} {np.floor(((v % 5) % 4) / 3):.0f} '
-              f'{np.floor((v % 5) / 4):.0f} {np.floor(v / 5):.0f}')
+        print(f'{(((v % 5) % 4) % 3) % 2} {np.floor((((v % 5) % 4) % 3) / 2).astype(int)} '
+              f'{np.floor(((v % 5) % 4) / 3).astype(int)} {np.floor((v % 5) / 4).astype(int)} '
+              f'{np.floor(v / 5).astype(int)}')
 
 
 if __name__ == '__main__':
