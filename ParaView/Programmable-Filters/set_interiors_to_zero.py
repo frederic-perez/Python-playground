5cbbd74b2dd92ad03f1361624a587dd5beabd253
Frederic Perez <frederic.perez@gmx.us>
2023-05-06 21:45:53 +0200

    ✨Add `set_interiors_to_zero` (WIP)

---
+++ ParaView/Programmable-Filters/set_interiors_to_zero.py
@@ -0,0 +1,46 @@
+"""module docstring should be here"""
+
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy
+import numpy as np
+
+input_data = self.GetInputDataObject(0, 0)
+image = vtk.vtkImageData.SafeDownCast(input_data)
+#print(image)
+point_data = image.GetPointData()
+array = image.GetPointData().GetArray(0)
+#print(array)
+max_label = int(array.GetRange()[1])
+num_tuples = array.GetNumberOfTuples()
+print(f'» max_label = {max_label}; #tuples = {num_tuples:,}')
+
+# Convert the VTK image to a numpy array
+numpy_array = vtk.util.numpy_support.vtk_to_numpy(image.GetPointData().GetScalars()).reshape(image.GetDimensions(), order='F')
+
+# Create a copy of the numpy array
+copy_numpy_array = np.copy(numpy_array)
+
+print(numpy_array.shape)
+
+# Iterate over each voxel in the numpy array
+for i in range(1, numpy_array.shape[0]-1):
+    for j in range(1, numpy_array.shape[1]-1):
+        for k in range(1, numpy_array.shape[2]-1):
+            # Check if value of voxel is the same as its neighbors
+            if numpy_array[i,j,k] == numpy_array[i-1,j,k] and numpy_array[i,j,k] == numpy_array[i+1,j,k] and numpy_array[i,j,k] == numpy_array[i,j-1,k] and numpy_array[i,j,k] == numpy_array[i,j+1,k] and numpy_array[i,j,k] == numpy_array[i,j,k-1] and numpy_array[i,j,k] == numpy_array[i,j,k+1]:
+                # Set value of voxel to zero in copy of numpy array
+                copy_numpy_array[i,j,k] = 0
+
+# Convert the copy of the numpy array back to a VTK image data structure
+vtk_copy_image = vtk.vtkStructuredPoints()
+vtk_copy_image.SetDimensions(copy_numpy_array.shape)
+vtk_copy_image.SetSpacing(1, 1, 1)
+vtk_copy_image.SetOrigin(0, 0, 0)
+vtk_copy_image.AllocateScalars(vtk_image.GetPointData().GetScalars().GetDataType(), 1)
+vtk.util.numpy_support.numpy_to_vtk(copy_numpy_array.ravel(order='F'), deep=True, array=vtk_copy_image.GetPointData().GetScalars())
+
+# Set the new scalar data on the output
+output = self.GetOutput()
+output.PointData.append(new_data, 'New Scalar Array')
+
+print('done')
