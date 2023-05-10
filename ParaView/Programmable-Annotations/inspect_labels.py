f3a817297a304782cb105c6f2153f6a1ba366411
Frederic Perez <frederic.perez@gmx.us>
2023-05-11 00:36:16 +0200

    💄Use a table + math in `inspect_labels`

--- ParaView/Programmable-Annotations/inspect_labels.py
+++ ParaView/Programmable-Annotations/inspect_labels.py
@@ -12,14 +12,11 @@ scalar_array = image.GetPointData().GetArray(0)
 #print(scalar_array)
 max_label = int(scalar_array.GetRange()[1])
 num_tuples = scalar_array.GetNumberOfTuples()
-string = f'max_label = {max_label}; #tuples = {num_tuples:,}'
+string = 'ID | # | %'  # f'max_label = {max_label}; #tuples = {num_tuples:,}'
 scalar_array_np = vtk.util.numpy_support.vtk_to_numpy(scalar_array)
 for label in range(0, max_label + 1):
     acc = np.count_nonzero(scalar_array_np == label)
-    if label == 0:
-        string += f'\nBackground: #"{label}"s = {acc:,} ({100*acc/num_tuples:.1f}%)'
-    else:
-        string += f'\n#"{label}"s = {acc:,} ({100*acc/num_tuples:.1f}%)'
+    string += f'\n{label} | {acc:,} | {100*acc/num_tuples:.1f}'
 
 to = self.GetTableOutput()
 arr = vtk.vtkStringArray()
