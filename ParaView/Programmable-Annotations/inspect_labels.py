91cd97dccd748f8902d3575aedafc0c4959ead6e
Frederic Perez <frederic.perez@gmx.us>
2023-05-11 17:08:31 +0200

    🐞💄Use math in `inspect_labels`

--- ParaView/Programmable-Annotations/inspect_labels.py
+++ ParaView/Programmable-Annotations/inspect_labels.py
@@ -12,7 +12,7 @@ scalar_array = image.GetPointData().GetArray(0)
 #print(scalar_array)
 max_label = int(scalar_array.GetRange()[1])
 num_tuples = scalar_array.GetNumberOfTuples()
-string = 'ID | # | %'  # f'max_label = {max_label}; #tuples = {num_tuples:,}'
+string = '$ID$ | # | %'  # f'max_label = {max_label}; #tuples = {num_tuples:,}'
 scalar_array_np = vtk.util.numpy_support.vtk_to_numpy(scalar_array)
 for label in range(0, max_label + 1):
     acc = np.count_nonzero(scalar_array_np == label)
