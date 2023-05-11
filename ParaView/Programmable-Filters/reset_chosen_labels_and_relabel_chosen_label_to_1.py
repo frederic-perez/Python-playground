7bbc801788a8fb2be1d69558dd69b3fcb01eabfc
Frederic Perez <frederic.perez@gmx.us>
2023-05-11 17:11:55 +0200

    🔨ENH script: chosen labels 2 0; relabel label 2 1

--- ParaView/Programmable-Filters/reset_chosen_labels_and_relabel_chosen_label_to_1.py
+++ ParaView/Programmable-Filters/reset_chosen_labels_and_relabel_chosen_label_to_1.py
@@ -1,29 +1,28 @@
 """module docstring should be here"""
 
 import vtk
-from vtk.util.numpy_support import vtk_to_numpy
-import numpy as np
 
 input_data = self.GetInputDataObject(0, 0)
 image = vtk.vtkImageData.SafeDownCast(input_data)
 #print(image)
 point_data = image.GetPointData()
 scalar_array = image.GetPointData().GetArray(0)
-print(scalar_array)
+#print(scalar_array)
 max_label = int(scalar_array.GetRange()[1])
 num_tuples = scalar_array.GetNumberOfTuples()
 print(f'» max_label = {max_label}; #tuples = {num_tuples:,}')
-label_to_reset = 1 #input('Label to reset: ')
+labels_to_reset = 1, 2, 3, 5
+label_to_relabel_to_1 = 4
 num_pixels_reset = 0
+new_array_np = vtk.util.numpy_support.vtk_to_numpy(scalar_array)
 for i in range(0, num_tuples):
-    if scalar_array.GetValue(i) == label_to_reset:
-        scalar_array.SetValue(i, 0)
+    label_value = scalar_array.GetValue(i)
+    if label_value in labels_to_reset:
+        new_array_np[i] = 0
         num_pixels_reset += 1
+    elif label_value == label_to_relabel_to_1:
+        new_array_np[i] = 1
 
 print(f'» #pixels_reset = {num_pixels_reset:,}')
 
-# Set the modified scalar array back to the input dataset
-input.GetPointData().SetScalars(array)
-
-# Set the output to be the same as the input
-output.ShallowCopy(input)
+output.PointData.append(new_array_np, "new_array")
