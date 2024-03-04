134645e24b9a17de7856b92b79024b0ce17d4278
frederic-perez <frederic.perez@gmx.us>
2024-03-05 00:17:47 +0100

    🐞Fix `Timer` tests after its petty refactoring

--- test_timer.py
+++ test_timer.py
@@ -16,14 +16,14 @@ class Test_timer(unittest.TestCase):
     def test_GivenATimerAnd0msPass_When_elapsed_ThenReturnElapsedCloseTo0ms(self):
         timer = Timer()
         result = timer.elapsed()
-        self.assertTrue(result == '0.00"' or result == '0.01"')
+        self.assertTrue(result == '0.0 seconds' or result == '0.1 seconds')
 
     def test_GivenATimerAndXmsPass_When_elapsed_ThenReturnElapsedCloseToXms(self):
         x_milliseconds = 0.123
         timer = Timer()
         sleep(x_milliseconds)
         result = timer.elapsed()
-        result_trimmed = result.replace("\"", "")
+        result_trimmed = result.replace(" seconds", "")
         it_is_close_enough = float(result_trimmed) - x_milliseconds <= 0.003
         if not it_is_close_enough:
             print(f'Too bad: result is {result}, too far from {x_milliseconds}')
