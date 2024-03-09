5cc32d743424ab72be3486564748f2b2655c3809
frederic-perez <frederic.perez@gmx.us>
2024-03-09 11:37:04 +0100

    🎨Simplify the parser using `choices=...` to ditch `help_for_parser` and `check_arguments`

--- cli_parser.py
+++ cli_parser.py
@@ -5,6 +5,7 @@ recommended command-line parsing module in the Python standard library.
 Info on argparse:
 - https://docs.python.org/3.12/library/argparse.html#argparse.ArgumentParser.parse_args
 - https://docs.python.org/3.12/howto/argparse.html#argparse-tutorial
+- https://realpython.com/command-line-interfaces-python-argparse/
 """
 
 import argparse
@@ -24,10 +25,6 @@ class PlatonicSolid(Enum):
 
 
 PlatonicSolidValues = tuple(member.value for member in PlatonicSolid)  # tuple is a MUST to reuse object
-
-
-def help_for_parser(enum_class):
-    return '{ ' + ', '.join(member.value for member in enum_class) + ' }'
 
 
 class Color(Enum):
@@ -59,11 +56,13 @@ OnOffValues = tuple(member.value for member in OnOff)  # tuple is a MUST to reus
 prog_name: Final = os.path.basename(__file__)
 
 epilog_text = \
-    (f'Parse a set of example mandatory and optional CLI arguments, capitalizing `word`, and squaring `number`.\n'
+    (f'Parse a set of example mandatory and optional CLI arguments, capitalizing `word`,\n'
+     'and squaring both `integer` and `double`.\n'
      'Usage examples:\n'
      f'1) python {prog_name} word 2 3.14 -i in.txt -o out.txt -u Jane\n'
      f'2) python {prog_name} -i in.txt -o out.txt -u Jane word 2 3.14\n'
-     f'3) python {prog_name} word 2 3.14 -i in -o out -u user --platonic-solid icosahedron --color green --fruit pear')
+     f'3) python {prog_name} word 2 3.14 -i in -o out -u user --platonic-solid {PlatonicSolid.icosahedron.value}'
+     f' --color {Color.green.value} --fruit {Fruit.pear.value}')
 
 
 def create_parser():
@@ -84,43 +83,37 @@ def create_parser():
     # 2) File selection
     #
     parser.add_argument(
-        '-i', '--input-file', dest='input_file', required=True, type=str, help='read input from FILE', metavar='FILE')
+        '-i', '--input-file', dest='input_file',
+        type=str, required=True,
+        help='read input from FILE', metavar='FILE')
     parser.add_argument(
-        '-o', '--output-file', dest='output_file', required=True, type=str, help='write output to FILE', metavar='FILE')
+        '-o', '--output-file', dest='output_file',
+        type=str, required=True,
+        help='write output to FILE', metavar='FILE')
 
     # 3) Operation flags/parameters
     #
-    parser.add_argument('-u', '--username', dest='username', required=True, type=str, help='your username')
-    parser.add_argument('--platonic-solid', dest='platonic_solid', type=str, default=PlatonicSolid.hexahedron.value,
-                        help=help_for_parser(PlatonicSolid))
-    parser.add_argument('--color', dest='color', type=str, default=Color.red.value, help=help_for_parser(Color))
-    parser.add_argument('--fruit', dest='fruit', type=str, default=Fruit.apple.value, help=help_for_parser(Fruit))
+    parser.add_argument(
+        '-u', '--username', dest='username',
+        type=str, required=True,
+        help='your username')
+    parser.add_argument(
+        '--platonic-solid', dest='platonic_solid',
+        type=str, choices=PlatonicSolidValues, required=False, default=PlatonicSolid.hexahedron.value)
+    parser.add_argument(
+        '--color', dest='color',
+        type=str, choices=ColorValues, required=False, default=Color.red.value)
+    parser.add_argument(
+        '--fruit', dest='fruit',
+        type=str, choices=FruitValues, required=False, default=Fruit.apple.value)
 
     # 4) Informative output
     #
-    parser.add_argument('-v', '--verbose', type=str, default=OnOff.off.value, help=help_for_parser(OnOff))
+    parser.add_argument(
+        '-v', '--verbose',
+        type=str, choices=OnOffValues, required=False, default=OnOff.off.value)
 
     return parser
-
-
-def check_arguments(args):
-    if not args.input_file:
-        return False
-    if not args.output_file:
-        return False
-    if args.platonic_solid not in PlatonicSolidValues:
-        print(f'❌ ERROR: Wrong platonic_solid ({args.platonic_solid}).\n')
-        return False
-    if args.color not in ColorValues:
-        print(f'❌ ERROR: Wrong color ({args.color}).\n')
-        return False
-    if args.fruit not in FruitValues:
-        print(f'❌ ERROR: Wrong fruit ({args.fruit}).\n')
-        return False
-    if args.verbose not in OnOffValues:
-        print(f'❌ ERROR: Wrong verbose ({args.verbose}).\n')
-        return False
-    return True
 
 
 def output_arguments(args):
@@ -146,9 +139,14 @@ def output_arguments(args):
 
 def deal_with_the_cli_parsing():
     parser = create_parser()
-    args = parser.parse_args()  # Parse known command-line options
-    succeeded = check_arguments(args)
-    if not succeeded:
+    try:
+        args = parser.parse_args()  # Parse known command-line options
+    except argparse.ArgumentError:
+        print('❌  ERROR: Caught `argparse.ArgumentError`')
+        parser.print_help()
+        sys.exit(1)
+    except SystemExit:
+        print('❌  ERROR: Caught `SystemExit`')
         parser.print_help()
         sys.exit(1)
     output_arguments(args)
