88919fcd81da3a3646c90fef694a0fdff8584192
frederic-perez <frederic.perez@gmx.us>
2024-03-09 18:02:24 +0100

    🎨Refactor and extend with argument groups and additional parameters

--- cli_parser.py
+++ cli_parser.py
@@ -24,16 +24,10 @@ class PlatonicSolid(Enum):
     dodecahedron = 'dodecahedron'
 
 
-PlatonicSolidValues = tuple(member.value for member in PlatonicSolid)  # tuple is a MUST to reuse object
-
-
 class Color(Enum):
     red = 'red'
     green = 'green'
     blue = 'blue'
-
-
-ColorValues = tuple(member.value for member in Color)  # tuple is a MUST to reuse object
 
 
 class Fruit(Enum):
@@ -42,97 +36,136 @@ class Fruit(Enum):
     pear = 'pear'
 
 
-FruitValues = tuple(member.value for member in Fruit)  # tuple is a MUST to reuse object
-
-
 class OnOff(Enum):
     on = 'on'
     off = 'off'
-
-
-OnOffValues = tuple(member.value for member in OnOff)  # tuple is a MUST to reuse object
 
 
 prog_name: Final = os.path.basename(__file__)
 
 epilog_text = \
     (f'Parse a set of example mandatory and optional CLI arguments, capitalizing `word`,\n'
-     'and squaring both `integer` and `double`.\n'
+     'and squaring all the input numbers.\n\n'
      'Usage examples:\n'
-     f'1) python {prog_name} word 2 3.14 -i in.txt -o out.txt -u Jane\n'
-     f'2) python {prog_name} -i in.txt -o out.txt -u Jane word 2 3.14\n'
-     f'3) python {prog_name} word 2 3.14 -i in -o out -u user --platonic-solid {PlatonicSolid.icosahedron.value}'
-     f' --color {Color.green.value} --fruit {Fruit.pear.value}')
+     f'1) python {prog_name} word 2 -3 4.5 -i in.txt -o out.txt -u Jane\n'
+     f'2) python {prog_name} -i in.txt word -o out.txt 2 -u Jane -3 4.5 # notice the interspersed mandatory arguments\n'
+     f'3) python {prog_name} word 2 -3 4.5 -i in -o out -u user --platonic-solid {PlatonicSolid.icosahedron.value}'
+     f' --color {Color.green.value} --fruit {Fruit.pear.value} --suggested-window-position 6 7')
+
+
+# Custom type function for negative integer
+def negative_integer(value):
+    int_value = int(value)
+    if int_value >= 0:
+        raise argparse.ArgumentTypeError(f"{value} is not a negative integer")
+    return int_value
+
+
+# Custom type function for unsigned integer
+def unsigned_integer(value):
+    try:
+        int_value = int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{value} is not an integer")
+    if int_value < 0:
+        raise argparse.ArgumentTypeError(f"{value} is not an unsigned integer")
+    return int_value
 
 
 def create_parser():
     # Create ArgumentParser instance
     parser = argparse.ArgumentParser(
+        add_help=False,  # disable the default help argument provided by argparse
+        allow_abbrev=False,
         description='A simple Python script to learn about parsing using the argparse module.',
-        epilog=epilog_text,
-        formatter_class=argparse.RawTextHelpFormatter)  # to preserve newlines and other formatting
+        formatter_class=argparse.RawTextHelpFormatter,  # to preserve newlines and other formatting
+        epilog=epilog_text)
 
     # Define known command-line options
 
-    # 1) Positional argument(s)
+    # 1) Positional arguments (mandatory)
     #
-    parser.add_argument('word', type=str)
-    parser.add_argument('integer', type=int)
-    parser.add_argument('float', type=float)
-
-    # 2) File selection
+    positional = parser.add_argument_group('1) Positional arguments (mandatory)')
+    positional.add_argument('word', metavar='<word>', type=str)
+    positional.add_argument('integer', metavar='<integer>', type=int)
+    positional.add_argument('negative_integer', metavar='<negative integer>', type=negative_integer)
+    positional.add_argument('float', metavar='<float>', type=float)
+    # 2) File selection (mandatory)
     #
-    parser.add_argument(
-        '-i', '--input-file', dest='input_file',
-        type=str, required=True,
-        help='read input from FILE', metavar='FILE')
-    parser.add_argument(
-        '-o', '--output-file', dest='output_file',
-        type=str, required=True,
-        help='write output to FILE', metavar='FILE')
+    file_selection = parser.add_argument_group('2) File selection (mandatory)')
+    file_selection.add_argument(
+        '-i', '--input-file', metavar='<file>', dest='input_file',
+        type=str,
+        required=True,
+        help='read input from <file>')
+    file_selection.add_argument(
+        '-o', '--output-file', metavar='<file>', dest='output_file',
+        type=str,
+        required=True,
+        help='write output to <file>')
 
     # 3) Operation flags/parameters
     #
-    parser.add_argument(
-        '-u', '--username', dest='username',
-        type=str, required=True,
-        help='your username')
-    parser.add_argument(
-        '--platonic-solid', dest='platonic_solid',
-        type=str, choices=PlatonicSolidValues, required=False, default=PlatonicSolid.hexahedron.value)
-    parser.add_argument(
-        '--color', dest='color',
-        type=str, choices=ColorValues, required=False, default=Color.red.value)
-    parser.add_argument(
-        '--fruit', dest='fruit',
-        type=str, choices=FruitValues, required=False, default=Fruit.apple.value)
+    operation_flags_and_parameters = parser.add_argument_group('3) Operation flags/parameters')
+    operation_flags_and_parameters.add_argument(
+        '-u', '--username', metavar='<string>', dest='username',
+        type=str,
+        required=True,
+        help='your username (mandatory)')
+    operation_flags_and_parameters.add_argument(
+        '--platonic-solid', choices=tuple(member.value for member in PlatonicSolid), dest='platonic_solid',
+        type=str,
+        required=False, default=PlatonicSolid.hexahedron.value,
+        help='platonic solid (optional)')
+    operation_flags_and_parameters.add_argument(
+        '--color', choices=tuple(member.value for member in Color), dest='color',
+        type=str,
+        required=False, default=Color.red.value,
+        help='color (optional)')
+    operation_flags_and_parameters.add_argument(
+        '--fruit', choices=tuple(member.value for member in Fruit), dest='fruit',
+        type=str,
+        required=False, default=Fruit.apple.value,
+        help='fruit (optional)')
+    operation_flags_and_parameters.add_argument(
+        '--suggested-window-position', metavar=('<i>', '<j>'),
+        type=unsigned_integer, nargs=2,
+        required=False, default=(1, 1),
+        help='in (unsigned integer) pixels, with `1 1` being the top-left corner (optional)')
 
     # 4) Informative output
     #
-    parser.add_argument(
-        '-v', '--verbose',
-        type=str, choices=OnOffValues, required=False, default=OnOff.off.value)
+    informative_output = parser.add_argument_group('4) Informative output (optional)')
+    informative_output.add_argument('-h', '--help', action='help', help='show this help message and exit')
+    informative_output.add_argument(
+        '-v', '--verbose', choices=tuple(member.value for member in OnOff),
+        type=str,
+        required=False, default=OnOff.off.value)
 
     return parser
 
 
 def output_arguments(args):
-    print('Positional argument(s):')
+    print(f'{prog_name} was called with the following options:')
+    print('')
+    print('1) Mandatory positional argument(s):')
     print(f'   word is {args.word}')
     print(f'   integer is {args.integer}')
+    print(f'   negative_integer is {args.negative_integer}')
     print(f'   float is {args.float}')
     print('')
-    print('File selection:')
+    print('2) File selection:')
     print(f'  --input-file {args.input_file}')
     print(f'  --output-file {args.output_file}')
     print('')
-    print('Operation flags/parameters:')
+    print('3) Operation flags/parameters:')
     print(f'  --username {args.username}')
     print(f'  --platonic-solid {args.platonic_solid}')
     print(f'  --color {args.color}')
     print(f'  --fruit {args.fruit}')
+    print(f'  --suggested-window-position {args.suggested_window_position[0]} {args.suggested_window_position[1]}')
     print('')
-    print('Informative output:')
+    print('4) Informative output:')
     print(f'   --verbose {args.verbose}')
     print('')
 
@@ -141,14 +174,13 @@ def deal_with_the_cli_parsing():
     parser = create_parser()
     try:
         args = parser.parse_args()  # Parse known command-line options
-    except argparse.ArgumentError:
-        print('❌  ERROR: Caught `argparse.ArgumentError`')
-        parser.print_help()
-        sys.exit(1)
     except SystemExit:
-        print('❌  ERROR: Caught `SystemExit`')
+        sys.exit(1)
+    except argparse.ArgumentError as e:
+        print(f'❌  ERROR: Caught `argparse.ArgumentError` {e}')
         parser.print_help()
         sys.exit(1)
+
     output_arguments(args)
     return args
 
@@ -156,6 +188,8 @@ def deal_with_the_cli_parsing():
 def do_the_actual_work(args):
     print(f'Capitalization of the word {args.word} is {args.word.upper()}.')
     print(f'The square of the integer {args.integer} is {args.integer * args.integer}.')
+    print(f'The square of the negative integer {args.negative_integer} is'
+          f' {args.negative_integer * args.negative_integer}.')
     print(f'The square of the float {args.float} is {args.integer * args.float}.')
 
 
@@ -163,10 +197,14 @@ def main():
     timer = Timer()
 
     args = deal_with_the_cli_parsing()
+
+    if args.verbose == OnOff.on.value:
+        print(f'The actual work is about to start...')
+
     do_the_actual_work(args)
 
     if args.verbose == OnOff.on.value:
-        print(f'Script finished in {timer.elapsed()}')
+        print(f'{prog_name} finished in {timer.elapsed()}.')
 
 
 if __name__ == '__main__':
