285cbd2c5a9075df48419e8c55195e103e10ac96
frederic-perez <frederic.perez@gmx.us>
2024-03-28 00:18:07 +0100

    🔨Learn the basics of `tkinter` 🚧2 (add radiobuttons for Enum members)

--- playground_tkinter.py
+++ playground_tkinter.py
@@ -1,20 +1,22 @@
 """module docstring should be here"""
 
-import tkinter
+import tkinter as tk
 
+from enum import Enum
 from typing import Final
 
 
 def play_with_tkinter():
     # Create tkinter window
-    root: Final = tkinter.Tk()
+    root: Final = tk.Tk()
     root.title('Menu Demo')
+    # root.geometry("500x250")
 
     # Create Menubar
-    menubar: Final = tkinter.Menu(root)
+    menubar: Final = tk.Menu(root)
 
     # Add File Menu and commands
-    file: Final = tkinter.Menu(menubar, tearoff=0)
+    file: Final = tk.Menu(menubar, tearoff=0)
     menubar.add_cascade(label='File', menu=file)
     file.add_command(label='New File', command='')
     file.add_command(label='Open...', command='')
@@ -23,7 +25,7 @@ def play_with_tkinter():
     file.add_command(label='Exit', command=root.destroy)  # Notice the command here
 
     # Add Edit Menu and commands
-    edit: Final = tkinter.Menu(menubar, tearoff=0)
+    edit: Final = tk.Menu(menubar, tearoff=0)
     menubar.add_cascade(label='Edit', menu=edit)
     edit.add_command(label='Cut', command='')
     edit.add_command(label='Copy', command='')
@@ -34,16 +36,35 @@ def play_with_tkinter():
     edit.add_command(label='Find again', command='')
 
     # Add Help Menu
-    help_: Final = tkinter.Menu(menubar, tearoff=0)
+    help_: Final = tk.Menu(menubar, tearoff=0)
     menubar.add_cascade(label='Help', menu=help_)
     help_.add_command(label='Tk Help', command='')
     help_.add_command(label='Demo', command='')
     help_.add_separator()
     help_.add_command(label='About Demo', command='')
 
+    # Define the radio buttons
+    class MontyPythonFood(Enum):
+        bacon = 'Bacon'
+        eggs = 'Eggs'
+        ham = 'Ham'
+        spam = 'Spam'
+
+    options: Final = tuple((member.name, member.value) for member in MontyPythonFood)
+    radio_value: Final = tk.StringVar(value=MontyPythonFood.spam.name)  # Let us set spam as the default
+    for value, text in options:
+        tk.Radiobutton(root, text=text, variable=radio_value, value=value).pack(anchor=tk.W)
+
+    def confirm():  # Function to be called when the user makes a selection
+        user_choice = radio_value.get()
+        print(f"Selected option {user_choice}")
+
+    # Button to confirm the selection
+    tk.Button(root, text="Confirm", command=confirm).pack()
+
     # Display Menu
     root.config(menu=menubar)
-    tkinter.mainloop()
+    tk.mainloop()
 
 
 def main():
