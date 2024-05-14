a694514a41d2502af66b2970bafadef53e9e96ea
Frederic Perez <frederic.perez@gmx.us>
2024-05-15 00:05:19 +0200

    🔨Add CLI parsing 🚧3

--- crop_1pixel_png_files.py
+++ crop_1pixel_png_files.py
@@ -4,7 +4,7 @@
 # Create an installer (a .exe file that will be placed in the dist/ subfolder) with the following command:
 # $ pyinstaller.exe --onefile crop_1pixel_png_files.py
 # Then you can create a shortcut on the Desktop to the newly created file .../dist/crop_1pixel_png_files.exe
-# that allows dragging and dropping to the corresponding icon the user's selected files to be cropped.
+# that allows dragging and dropping to the corresponding icon the user's selected files to be treated.
 
 import argparse
 import colorama
@@ -146,51 +146,50 @@ def calculate_percentage_reduction(size_a: int, size_b: int) -> float:
     return percentage_reduction
 
 
-def process_file(the_file_path_original: str, acc: int) -> None:
-    # Open the original image
-    image_original: Final = Image.open(the_file_path_original)
+def process_file(args: argparse.Namespace, the_file_path_original: str, acc: int) -> None:
+    image_original: Final = Image.open(the_file_path_original)  # Open the original image
     width_original, height_original = image_original.size
 
-    # Crop the image
-    image_cropped: Final = crop_image(image_original)
-    width_cropped, height_cropped = image_cropped.size
+    image_treated = crop_image(image_original) if args.crop == OnOff.on.value else image_original
+
+    width_treated, height_treated = image_treated.size
 
     # Construct the file path for the new image by adding a suffix before the extension
     base, ext = os.path.splitext(the_file_path_original)
     file_path_treated: Final[str] = f"{base}-TRTD{ext}"  # https://acronyms.thefreedictionary.com/trtd » Treated
 
-    # Save the cropped image
-    image_cropped.save(file_path_treated)
+    # Save the treated image
+    image_treated.save(file_path_treated)
 
     basename_original: Final[str] = os.path.basename(the_file_path_original)
     basename_base, _ = os.path.splitext(basename_original)
     size_original: Final[int] = os.path.getsize(the_file_path_original)
-    size_cropped: Final[int] = os.path.getsize(file_path_treated)
+    size_treated: Final[int] = os.path.getsize(file_path_treated)
     size_str_original: Final[str] = get_human_readable_size(size_original)
-    size_str_cropped: Final[str] = get_human_readable_size(size_cropped)
-    percent_reduction: Final[float] = calculate_percentage_reduction(size_original, size_cropped)
+    size_str_treated: Final[str] = get_human_readable_size(size_treated)
+    percent_reduction: Final[float] = calculate_percentage_reduction(size_original, size_treated)
     print(Back.LIGHTYELLOW_EX + Fore.BLACK + f' #{acc:03d} ' + Style.RESET_ALL + ' ' +
           Fore.LIGHTCYAN_EX + f'{basename_base}' +
           Back.LIGHTYELLOW_EX + Fore.BLACK + f' -TRTD ' + Style.RESET_ALL + Fore.LIGHTCYAN_EX + f'{ext}' +
           Style.RESET_ALL + ': ' +
           'From ' + Fore.LIGHTCYAN_EX + f'{width_original}x{height_original} ({size_str_original}) ' + Style.RESET_ALL +
-          'to ' + Back.LIGHTYELLOW_EX + Fore.BLACK + f' {width_cropped}x{height_cropped} ({size_str_cropped}' +
+          'to ' + Back.LIGHTYELLOW_EX + Fore.BLACK + f' {width_treated}x{height_treated} ({size_str_treated}' +
           ' ' + emoji.emojize(':down_arrow:') + f'{percent_reduction:.1f}%) ')
 
 
-def process_hardcoded(folder_path: str, pattern: str) -> None:
+def process_hardcoded(args: argparse.Namespace, folder_path: str, pattern: str) -> None:
     # Traverse the folder and print the names of PNG files that satisfy the pattern
     acc: int = 0
     for file_name in os.listdir(folder_path):
         if re.match(pattern, file_name):
             acc += 1
             file_path_original: str = folder_path + '/' + file_name
-            process_file(file_path_original, acc)
+            process_file(args, file_path_original, acc)
         # if acc > 5:
         #    break
 
 
-def process_files(file_paths: list[str]) -> None:
+def process_files(args: argparse.Namespace, file_paths: list[str]) -> None:
     if len(file_paths) == 0:
         return
 
@@ -200,7 +199,7 @@ def process_files(file_paths: list[str]) -> None:
     acc: int = 0
     for file_path_original in file_paths:
         acc += 1
-        process_file(file_path_original, acc)
+        process_file(args, file_path_original, acc)
     # if acc > 5:
     #    break
 
@@ -221,9 +220,9 @@ if __name__ == '__main__':
         print("No files provided » processing 'hardcoded' files:")
         hardcoded_folder_path: Final[str] = "D:\\3phemeral/foo-TRTD"
         hardcoded_pattern: Final[str] = r"^\d\d (DE|EN|ES) snap-\d{6}-\d{4}.png"
-        process_hardcoded(hardcoded_folder_path, hardcoded_pattern)
+        process_hardcoded(args, hardcoded_folder_path, hardcoded_pattern)
     else:
-        process_files(unknown_args)
+        process_files(args, unknown_args)
 
     if args.verbose == OnOff.on.value:
         print('\n' + Style.BRIGHT + f'{prog_name}' + Style.RESET_ALL + f' finished in {timer.elapsed()}.')
