4da0fa47001c5a78be2e2755975f21897422fb1c
Frederic Perez <frederic.perez@gmx.us>
2024-05-18 13:59:12 +0200

    🔨Learn the basics of `pyautogui` 🚧1

---
+++ auto_gui.py
@@ -0,0 +1,94 @@
+"""module docstring should be here"""
+
+import gettext
+import os
+import pyautogui as pg
+import pyperclip
+import time
+
+from colorama import Fore, Style
+from enum import Enum
+from typing import Final
+
+title_FYI: Final[str] = '🤖💬 FYI'
+
+
+def go_to_the_middle_of_the_window_and_do_click() -> None:
+    # noinspection PyUnresolvedReferences
+    pg.alert('🤖💬 I am going to move the cursor to the middle of the screen, and print its location',
+             title=title_FYI)
+    screen_width, screen_height = pg.size()
+    pg.moveTo(screen_width / 2, screen_height / 2, duration=.25)
+    print(pg.position())
+
+
+# Function to paste text using PyAutoGUI and Pyperclip
+def paste_text(text: str) -> None:
+    pyperclip.copy(text)
+    pg.hotkey('ctrl', 'v')  # Simulate paste action
+
+
+def play_a_bit_with_notepad_plus_plus() -> None:
+    # noinspection PyUnresolvedReferences
+    pg.alert('🤖💬 I am going to open notepad++.exe, create a new buffer, type something, and quit without saving.',
+             title=title_FYI)
+    pg.PAUSE = 1
+    pg.press('win')
+    pg.typewrite('notepad', interval=.025)
+    paste_text('++')
+    pg.typewrite('.exe', interval=.025)
+    pg.press('enter')
+
+    pg.hotkey('ctrl', 'n')  # 'New'
+    pg.typewrite('Hello, world!\nI will close this tab in 1 second...', interval=.025)
+    time.sleep(1)
+    pg.hotkey('ctrl', 'w')  # 'Close'
+    pg.hotkey('tab')
+    pg.press('enter')
+    pg.hotkey('alt', 'f4')  # 'Exit'
+
+
+def translate_hello_world() -> None:
+    # Set up the location of the .mo files
+    localedir: Final[str] = os.path.join(os.path.dirname(__file__), 'locales')
+
+    # Choose the domain (name of the .mo files)
+    domain: Final[str] = 'hello'
+
+    class Languages(Enum):
+        de = 'de'
+        emoji = 'emoji'  # for the sort of fake emoji language
+        en = 'en'
+        fr = 'fr'
+        ja = 'ja'
+
+    languages_str: Final[tuple[str, ...]] = tuple(member.value for member in Languages)
+
+    # Create translation instances
+    map_language_to_translator: dict[str, gettext.GNUTranslations | gettext.NullTranslations] = {}
+    for language_str in languages_str:
+        map_language_to_translator[language_str] = gettext.translation(domain, localedir, [language_str], fallback=True)
+
+    list_of_languages_plus_quit = list(member.value for member in Languages)
+    list_of_languages_plus_quit.append('Quit')
+    while True:
+        # noinspection PyUnresolvedReferences
+        language_str = pg.confirm(text='🤖💬 Choose the target language to translate Hello, world! to',
+                                  title='Language selection',
+                                  buttons=list_of_languages_plus_quit)
+        if language_str in languages_str:
+            map_language_to_translator[language_str].install()  # The 'magic' happens here
+            print('» ' + Fore.LIGHTYELLOW_EX + f'{language_str}' + Style.RESET_ALL + ': ' +
+                  _('Hello, world!'))  # type: ignore[name-defined]
+        else:
+            break
+
+
+def main():
+    go_to_the_middle_of_the_window_and_do_click() if True else None
+    play_a_bit_with_notepad_plus_plus() if True else None
+    translate_hello_world() if True else None
+
+
+if __name__ == '__main__':
+    main()
