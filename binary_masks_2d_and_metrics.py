f94fb7f1a11afc5472d6560ebc0569b81ce0a868
frederic-perez <frederic.perez@gmx.us>
2024-06-30 17:54:33 +0200

    🔨Replicate stuff from Reinke et al. (2027) _wrt_ 2d binary masks and metrics`

---
+++ binary_masks_2d_and_metrics.py
@@ -0,0 +1,191 @@
+"""module docstring should be here"""
+
+import nibabel as nib
+import numpy as np
+import tempfile
+
+from skimage.morphology import skeletonize, skeletonize_3d
+from typing import Final
+
+
+def format_float_as_in_paper(x: float) -> str:
+    return '{0:.2f}'.format(x).rstrip('.')
+
+
+def ascii_to_binary_mask_2d(ascii_file_path: str) -> np.ndarray:
+    """
+    Converts an ASCII file representing a binary mask 2D to a binary 2D numpy array.
+
+    Args:
+        ascii_file_path (str): Path to the ASCII file.
+
+    Returns:
+        numpy.ndarray: A 2D binary mask where 0 represents background and 1 represents foreground.
+    """
+
+    with open(ascii_file_path, "r") as f:
+        lines = f.readlines()
+
+    # Determine dimensions of the image
+    height: Final[int] = len(lines)
+    width: Final[int] = max(len(line.strip()) for line in lines)
+
+    # Create an empty numpy array
+    mask = np.zeros((height, width), dtype=np.uint8)
+
+    # Iterate through each line and character
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line.strip()):
+            # Set pixel to 1 if it's a non-whitespace character or '0'
+            if char != ' ' and char != '0':
+                mask[y, x] = 1
+
+    return mask
+
+
+def precision(mask1: np.ndarray, mask2: np.ndarray) -> float:
+    intersection = np.logical_and(mask1, mask2)
+    result = intersection.sum() / mask2.sum()
+    return result
+
+
+def recall(mask1: np.ndarray, mask2: np.ndarray) -> float:
+    intersection = np.logical_and(mask1, mask2)
+    result = intersection.sum() / mask1.sum()
+    return result
+
+
+def dice_coefficient_a(mask1: np.ndarray, mask2: np.ndarray) -> float:
+    intersection: Final = np.logical_and(mask1, mask2)
+    result: Final = (2.0 * intersection.sum()) / (mask1.sum() + mask2.sum())
+    return result
+
+
+def dice_coefficient_b(precision_value: float, recall_value: float) -> float:
+    result: Final = 2 * (precision_value * recall_value) / (precision_value + recall_value)
+    return result
+
+
+def cl_score(v, s):
+    """[this function computes the skeleton volume overlap]
+
+    Args:
+        v ([bool]): [image]
+        s ([bool]): [skeleton]
+
+    Returns:
+        [float]: [computed skeleton volume intersection]
+    """
+    return np.sum(v*s)/np.sum(s)
+
+
+def cl_dice(v_p, v_l):
+    """[this function computes the cldice metric]
+
+    Args:
+        v_p ([bool]): [predicted image]
+        v_l ([bool]): [ground truth image]
+
+    Returns:
+        [float]: [cldice metric]
+    """
+    if len(v_p.shape)==2:
+        tprec = cl_score(v_p,skeletonize(v_l))
+        tsens = cl_score(v_l,skeletonize(v_p))
+    elif len(v_p.shape)==3:
+        tprec = cl_score(v_p,skeletonize_3d(v_l))
+        tsens = cl_score(v_l,skeletonize_3d(v_p))
+    return 2*tprec*tsens/(tprec+tsens)
+
+
+def replicate_reinke_2024_extended_data_fig_1_p2_2a(top_or_bottom: str, spy: bool) -> None:
+    identity: Final = np.eye(4)  # Used to save NIFTI files (if the conditions to do so are met)
+    temp_dir = tempfile.gettempdir()  # Used to save NIFTI files (if the conditions to do so are met)
+
+    basename_file_mask_2d: Final[str] = 'binary-mask-2d-Extended-Data-Fig-1-P2-2a-' + top_or_bottom
+
+    basename_file_mask_2d_reference: Final[str] = basename_file_mask_2d + '-Reference'
+    filename_ascii_mask_2d_reference: Final[str] = 'data/Reinke-2024/' + basename_file_mask_2d_reference + ".txt"
+    mask_2d_reference: Final[np.ndarray] = ascii_to_binary_mask_2d(filename_ascii_mask_2d_reference)
+    if spy:
+        print(f'mask_2d_reference =\n{mask_2d_reference}')
+        mask_2d_nifti = nib.Nifti1Image(mask_2d_reference, identity)  # Create NIfTI1 image object
+        filename_nifti_mask_2d = temp_dir + "/" + basename_file_mask_2d_reference + ".nii.gz"
+        nib.save(mask_2d_nifti, filename_nifti_mask_2d)  # Save the NIFTI objects to file
+
+    for i in 1, 2:
+        basename_file_mask_2d_prediction = basename_file_mask_2d + '-Prediction-' + str(i)
+        filename_ascii_mask_2d_prediction = 'data/Reinke-2024/' + basename_file_mask_2d_prediction + ".txt"
+        mask_2d_prediction = ascii_to_binary_mask_2d(filename_ascii_mask_2d_prediction)
+        if spy:
+            print(f'mask_2d_prediction #{i} =\n{mask_2d_prediction}')
+            mask_2d_nifti = nib.Nifti1Image(mask_2d_prediction, identity)  # Create NIfTI1 image object
+            filename_nifti_mask_2d = temp_dir + "/" + basename_file_mask_2d_prediction + ".nii.gz"
+            nib.save(mask_2d_nifti, filename_nifti_mask_2d)  # Save the NIFTI objects to file
+
+        # Compute the Dice coefficient
+        dice_a = dice_coefficient_a(mask_2d_reference, mask_2d_prediction)
+        dice_b = dice_coefficient_b(
+            precision(mask_2d_reference, mask_2d_prediction),
+            recall(mask_2d_reference, mask_2d_prediction))
+
+        print(f'Dice({filename_ascii_mask_2d_reference}, {filename_ascii_mask_2d_prediction}) = '
+              f'{format_float_as_in_paper(dice_a)} or {format_float_as_in_paper(dice_b)}')
+
+
+def replicate_reinke_2024_extended_data_fig_1_p2_2b(spy: bool) -> None:
+    identity: Final = np.eye(4)  # Used to save NIFTI files (if the conditions to do so are met)
+    temp_dir = tempfile.gettempdir()  # Used to save NIFTI files (if the conditions to do so are met)
+
+    basename_file_mask_2d: Final[str] = 'binary-mask-2d-Extended-Data-Fig-1-P2-2b'
+
+    basename_file_mask_2d_reference: Final[str] = basename_file_mask_2d + '-Reference'
+    filename_ascii_mask_2d_reference: Final[str] = 'data/Reinke-2024/' + basename_file_mask_2d_reference + ".txt"
+    mask_2d_reference: Final[np.ndarray] = ascii_to_binary_mask_2d(filename_ascii_mask_2d_reference)
+    if spy:
+        print(f'mask_2d_reference =\n{mask_2d_reference}')
+        mask_2d_nifti = nib.Nifti1Image(mask_2d_reference, identity)  # Create NIfTI1 image object
+        filename_nifti_mask_2d = temp_dir + "/" + basename_file_mask_2d_reference + ".nii.gz"
+        nib.save(mask_2d_nifti, filename_nifti_mask_2d)  # Save the NIFTI objects to file
+
+    for i in 1, 2:
+        basename_file_mask_2d_prediction = basename_file_mask_2d + '-Prediction-' + str(i)
+        filename_ascii_mask_2d_prediction = 'data/Reinke-2024/' + basename_file_mask_2d_prediction + ".txt"
+        mask_2d_prediction = ascii_to_binary_mask_2d(filename_ascii_mask_2d_prediction)
+        if spy:
+            print(f'mask_2d_prediction #{i} =\n{mask_2d_prediction}')
+            mask_2d_nifti = nib.Nifti1Image(mask_2d_prediction, identity)  # Create NIfTI1 image object
+            filename_nifti_mask_2d = temp_dir + "/" + basename_file_mask_2d_prediction + ".nii.gz"
+            nib.save(mask_2d_nifti, filename_nifti_mask_2d)  # Save the NIFTI objects to file
+
+        # Compute the Dice coefficient
+        dice_a = dice_coefficient_a(mask_2d_reference, mask_2d_prediction)
+        dice_b = dice_coefficient_b(
+            precision(mask_2d_reference, mask_2d_prediction),
+            recall(mask_2d_reference, mask_2d_prediction))
+        cl_dice_value = cl_dice(mask_2d_reference, mask_2d_prediction)
+        print(f'Dice({filename_ascii_mask_2d_reference}, {filename_ascii_mask_2d_prediction}) = '
+              f'{format_float_as_in_paper(dice_a)} or {format_float_as_in_paper(dice_b)}; '
+              f'clDice = {format_float_as_in_paper(cl_dice_value)}')
+
+
+def replicate_reinke_2024_extended_data_fig_1_p2_2() -> None:
+    """
+    Here we sort of replicate the very interesting stuff from this article:
+    Reinke, A., Tizabi, M.D., Baumgartner, M. et al. Understanding metric-related pitfalls in image analysis validation.
+    Nat Methods 21, 182–194 (2024). https://doi.org/10.1038/s41592-023-02150-0,
+    concretely its Extended Data's Fig. 1's P2.2
+    """
+    spy = False  # True
+
+    for top_or_bottom in 'top', 'bottom':
+        replicate_reinke_2024_extended_data_fig_1_p2_2a(top_or_bottom, spy)
+    replicate_reinke_2024_extended_data_fig_1_p2_2b(spy)
+
+
+def main():
+    replicate_reinke_2024_extended_data_fig_1_p2_2()
+
+
+if __name__ == '__main__':
+    main()
